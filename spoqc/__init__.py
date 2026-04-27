4e9ddf30ed12c5717b6df7e3c77a3089fdb3dba3
f641l <florian.heyl@dkfz-heidelberg.de>
2026-04-27 12:26:14 +0200

    version bum in __init__.py

--- spoqc/__init__.py
+++ spoqc/__init__.py
@@ -4,6 +4,6 @@ try:
     from importlib.metadata import version as _version
     __version__ = _version("spoqc")
 except Exception:  # pragma: no cover
-    __version__ = "0.0.0"
+    __version__ = "0.0.1"
 
 __all__ = ["__version__"]
