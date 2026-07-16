71292ac8ad0bba1793f7875ff0e3d7f885bdf276
f641l <florian.heyl@dkfz-heidelberg.de>
2026-07-16 15:20:55 +0200

    adding new std argument to pixel_score.py

--- spoqc/priors/hqpr/pixel_score.py
+++ spoqc/priors/hqpr/pixel_score.py
@@ -5,7 +5,7 @@ from scipy.stats import norm
 from sklearn.mixture import GaussianMixture
 from dask_ml.preprocessing import MinMaxScaler
 
-def calc_probs_pixel_score(pixel_scores, gmm_mod=3, nstds=None, t=None):
+def calc_probs_pixel_score(pixel_scores, gmm_mod=3, nstds=None, t=None, std=None):
     mix = GaussianMixture(n_components=gmm_mod, tol=1e-8, max_iter=int(1e4))
     mix.fit(pixel_scores.reshape(-1, 1))
     means = mix.means_
@@ -19,6 +19,9 @@ def calc_probs_pixel_score(pixel_scores, gmm_mod=3, nstds=None, t=None):
         max_std = 1.0  # Since mean is hard picked, we will use unit variance.
     else:
         max_mean = np.max(means)
+
+    if ( std ):
+        max_std = std
 
     print(f'Using std {max_std} and mean {max_mean} for pixel prior')
 
