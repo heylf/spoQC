f6d1bac72f00c25d5e2c016ca5f12758f4128114
f641l <florian.heyl@dkfz-heidelberg.de>
2026-08-19 20:09:04 +0200

    adding new invlig geometries and negatvie probes priors, chagnes to combine_priros, invalid_geom, negativE_probes_count.py

--- spoqc/priors/combine_priors.py
+++ spoqc/priors/combine_priors.py
@@ -8,16 +8,37 @@ from .. import helperfuncs
 # We will combine the pixel scorep prior with more priors
 def combine_priors_hqcr(sdata, figure_path, cell_df, qc_domains_adata, counts):
 
-    prior_transcript_counts, cell_df = priors.hqcr.transcript_counts.calc_transcript_counts_probs(
+    prior_transcript_counts, cell_df = priors.hqcr.transcript_and_gene_counts.calc_counts_probs(
         sdata, 
         figure_path,
         cell_df,
         qc_domains_adata,
-        counts
+        counts,
+        1.0,
+    )
+    prior_gene_counts, cell_df = priors.hqcr.transcript_and_gene_counts.calc_counts_probs(
+        sdata, 
+        figure_path,
+        cell_df,
+        qc_domains_adata,
+        "n_genes_by_counts",
+        0.5,
     )
     prior_doublet_distance = priors.hqcr.doublet_distance.calc_probs_doublet_distance(sdata, figure_path, nstds=100)
+    prior_negative_probe_counts = priors.hqcr.negative_probe_counts.calc_probs(
+        cell_df,
+        figure_path
+    )
+    prior_invalid_cell_geometry = priors.hqcr.invalid_geometry.calc_probs(sdata, figure_path, 'cell')
+    prior_invalid_nucelus_geometry = priors.hqcr.invalid_geometry.calc_probs(sdata, figure_path, 'nucleus')
 
-    final_prior = prior_transcript_counts + prior_doublet_distance
+    final_prior = \
+        prior_transcript_counts + \
+        prior_gene_counts + \
+        prior_doublet_distance + \
+        prior_negative_probe_counts + \
+        prior_invalid_cell_geometry + \
+        prior_invalid_nucelus_geometry
     final_prior = helperfuncs.min_max_normalize(final_prior)
 
     sdata['table'].obs['good_quality_probabilities'] = final_prior
---
+++ spoqc/priors/hqcr/negative_probe_counts.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+
+from ... import helperfuncs
+
+from scipy.stats import norm
+from sklearn.mixture import GaussianMixture
+from dask_ml.preprocessing import MinMaxScaler
+
+def calc_probs(df, figure_path, gmm_mod=1, nstds=1, t=1, std=1, tail="right"):
+    values = np.array(df["control_probe_counts"])
+    mix = GaussianMixture(n_components=gmm_mod, tol=1e-8, max_iter=int(1e4))
+    mix.fit(values.reshape(-1, 1))
+    means = mix.means_
+    cov = mix.covariances_
+    stds = [ np.sqrt(np.trace(cov[i])) for i in range(0, gmm_mod) ]
+    max_std = stds[np.argmax(means)]
+
+    max_mean = -1
+    if ( t ):
+        max_mean = t
+        max_std = 1.0  # Since mean is hard picked, we will use unit variance.
+    else:
+        max_mean = np.max(means)
+
+    if ( std ):
+        max_std = std
+
+    print(f'Using std {max_std} and mean {max_mean} for pixel prior and tail {tail}')
+
+    helperfuncs.plot_histogram_for_array(
+        values,
+        20,
+        figure_path,
+        f"Negative probes: t={np.round(max_mean, 3)} with {nstds} x {np.round(max_std, 3)} std & {tail} tail filtering",
+        "negative_probes_prior",
+        t=max_mean,
+        std=max_std,
+        nstds=nstds,
+    )
+
+    pdf = norm.pdf(values, loc=max_mean, scale=nstds*max_std)
+
+    # Just a trick, if values are bigger or smaller based on tail then set those values to t
+    # and thus get the best probability for all those values.
+    # This is basically to enforce 
+    if tail == "left":
+        pdf = np.where(values < max_mean, np.max(pdf), pdf)
+    elif tail == "right":
+        pdf = np.where(values > max_mean, np.max(pdf), pdf)
+
+    out = np.max(pdf) - pdf
+
+    # Calculate the probability at x for each pixel clusters.
+    return helperfuncs.min_max_normalize(out)
