20b6c51dcee147e0cf748cc2f351eea6545079de
f641l <florian.heyl@dkfz-heidelberg.de>
2026-08-20 15:01:51 +0200

    speed up global morans I calculation, changes to global_moran_I.py and qc_ambient.py

--- spoqc/subworkflows/qc_ambient.py
+++ spoqc/subworkflows/qc_ambient.py
@@ -1,11 +1,15 @@
 
 from .. import metrics
 
-def start_qc_ambient(sdata, figure_path, spoqc_tmp_folder, threads):
+def start_qc_ambient(sdata, figure_path, spoqc_tmp_folder):
     sdata['table'].X = sdata['table'].layers['normlog']
 
     print("[Note] Investigate global ambient contamination")
-    global_ambient = metrics.transcript_density.global_moran_I.calculate_global_moran_I_values(sdata, figure_path, spoqc_tmp_folder, threads)
+    global_ambient = metrics.transcript_density.global_moran_I.calculate_global_moran_I_values(
+        sdata,
+        figure_path,
+        spoqc_tmp_folder
+    )
 
     print("[finish]")
     return global_ambient
