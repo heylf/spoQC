6d2e98f7d2cb1fbd3cfc80f31fc0d17abba0aaa7
f641l <florian.heyl@dkfz-heidelberg.de>
2026-08-27 15:21:36 +0200

    change to hqcr.py

--- spoqc/subworkflows/hqcr.py
+++ spoqc/subworkflows/hqcr.py
@@ -492,14 +492,14 @@ def load_data_for_hqcr(sdata, spoqc_tmp_folder, counts):
     return qc_domains_adata, cell_df, qc_metrices
 
 
-def clustering_for_hqcr(qc_domains_adata, figure_path, seed, test_res_n_clusters=10, test_res=False):
+def clustering_for_hqcr(qc_domains_adata, figure_path, CONST, seed, test_res_n_clusters=10, test_res=False):
     # leiden clustering
     print("[NOTE] Cell QC clustering")
     sc.pp.neighbors(qc_domains_adata, n_neighbors=20, random_state=seed)
     sc.tl.umap(qc_domains_adata, random_state=seed)
 
     if ( test_res ):
-        ss = helperfuncs.test_resolutions_leiden(qc_domains_adata, figure_path, test_res_n_clusters)
+        helperfuncs.test_resolutions_leiden(qc_domains_adata, figure_path, CONST.THREADS, k=test_res_n_clusters)
 
     sc.tl.leiden(qc_domains_adata, resolution=1.2)
 
@@ -529,7 +529,7 @@ def start_hqcr(sdata, spoqc_tmp_folder, imagedim, CONST, seed):
         counts = 'canorm_transcript_counts'
 
     qc_domains_adata, cell_df, qc_metrices = load_data_for_hqcr(sdata, spoqc_tmp_folder, counts)
-    clustering_for_hqcr(qc_domains_adata, figure_path, seed)
+    clustering_for_hqcr(qc_domains_adata, figure_path, CONST, seed)
     
     # Here we combine available priors
     priors.combine_priors.combine_priors_hqcr(sdata, figure_path, cell_df, qc_domains_adata, counts)
